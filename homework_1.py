46e49e1dad3fa2eab8d8957521864c95cecdfe9b
Samil Akpinar <samilakpinar6@gmail.com>
2020-12-23 23:34:02 +0300

    homework

---
+++ homework_1.py
@@ -0,0 +1,18 @@
+num_1 = int(input("Please enter a integer number: "))
+num_2 = float(input("Please enter a float number: "))
+num_3 = input("Please enter a string value: ")
+num_4 = int(input("Please enter a integer number: "))
+num_5 = input("Please enter a string value: ")
+
+print("\nFirst entered value: {} ".format(num_1))
+print("Second entered value: {} ".format(num_2))
+print("Third entered value: {} ".format(num_3))
+print("Fourth entered value: {} ".format(num_4))
+print("fifth entered value: {} ".format(num_5))
+
+
+print(f"\nThe type of value: {type(num_1)}")
+print(f"The type of value: {type(num_2)}")
+print(f"The type of value: {type(num_3)}")
+print(f"The type of value: {type(num_4)}")
+print(f"The type of value: {type(num_5)}")
